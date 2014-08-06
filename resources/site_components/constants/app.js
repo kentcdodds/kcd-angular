4947fa5fa1fb59ac0e2c6f1e8e6679e3fa6091a4
Kent C. Dodds <kent@doddsfamily.us>
2014-08-06 00:08:38 -0600

    Bringing in Faker

---
+++ resources/site_components/constants/app.js
@@ -0,0 +1,11 @@
+(function() {
+  'use strict';
+
+  var app = angular.module('kcd.site');
+  function addConstant(name) {
+    if (window[name]) {
+      app.constant(name, window[name]);
+    }
+  }
+  addConstant('faker');
+})();
