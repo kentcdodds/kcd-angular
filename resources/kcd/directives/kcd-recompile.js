30fd2a5bd34cc7630d40368d4143791e8e45a03e
Kent C. Dodds <kent@doddsfamily.us>
2014-08-06 23:37:45 -0600

    Breaking change. Now will recompile everything from itself on down so we can destroy the scope and recreate the template... Not sure this is the best approach, but it fixes a major bug where we'd have dangling scopes and watchers hanging around...

--- resources/kcd/directives/kcd-recompile.js
+++ resources/kcd/directives/kcd-recompile.js
@@ -1,41 +1,33 @@
 angular.module('kcd.directives').directive('kcdRecompile', function($compile, $parse) {
   'use strict';
-
   return {
     scope: true, // required to be able to clear watchers safely
     compile: function(el) {
-      var template = el.html();
+      var template = getElementAsHtml(el);
       return function link(scope, $el, attrs) {
-        scope.$parent.$watch(attrs.kcdRecompile, function(_new, _old) {
+        var stopWatching = scope.$parent.$watch(attrs.kcdRecompile, function(_new, _old) {
           var useBoolean = attrs.hasOwnProperty('useBoolean');
           if ((useBoolean && (!_new || _new === 'false')) || (!useBoolean && (!_new || _new === _old))) {
             return;
           }
-          // remove all watchers because the recompiled version will set them up again.
-          removeChildrenWatchers($el);
           // reset kcdRecompile to false if we're using a boolean
           if (useBoolean) {
             $parse(attrs.kcdRecompile).assign(scope.$parent, false);
           }
 
           // recompile
-          var newEl = $compile(template)(scope.$parent.$new());
-          $el.html(newEl);
+          var newEl = $compile(template)(scope.$parent);
+          $el.replaceWith(newEl);
+
+          // Destroy old scope, reassign new scope.
+          stopWatching();
+          scope.$destroy();
         });
       };
     }
   };
 
-  function removeChildrenWatchers(element) {
-    angular.forEach(element.children(), function(childElement) {
-      removeAllWatchers(angular.element(childElement));
-    });
-  }
-
-  function removeAllWatchers(element) {
-    if (element.data().hasOwnProperty('$scope')) {
-      element.data().$scope.$$watchers = [];
-    }
-    removeChildrenWatchers(element);
+  function getElementAsHtml(el) {
+    return angular.element('<a></a>').append(el.clone()).html();
   }
 });
