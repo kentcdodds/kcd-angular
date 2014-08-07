e96fb229b4d6d2de277aff72cfeb734d73e0289f
Kent C. Dodds <kent@doddsfamily.us>
2014-08-06 23:36:43 -0600

    This was calculating watches incorrectly... Fixed...

--- resources/site_components/directives/kcd-watch-count.js
+++ resources/site_components/directives/kcd-watch-count.js
@@ -18,15 +18,15 @@ angular.module('kcd.site').directive('kcdWatchCount', function() {
         var watcherCount = 0;
 
         function getWatchers(element) {
-          if (element.data().hasOwnProperty('$scope')) {
-            var watchers = element.data().$scope.$$watchers || [];
-            watcherCount += watchers.length;
-            var isKcdWatchCountWatcher = watchers.some(function(watcher) {
-              return watcher.exp === watchCountWatcher;
-            });
-            if (isKcdWatchCountWatcher) {
-              watcherCount = watcherCount - 1;
-            }
+          var isolateWatchers = getWatchersFromScope(element.data().$isolateScope);
+          var scopeWatchers = getWatchersFromScope(element.data().$scope);
+          var watchers = scopeWatchers.concat(isolateWatchers);
+          watcherCount += watchers.length;
+          var isKcdWatchCountWatcher = watchers.some(function(watcher) {
+            return watcher.exp === watchCountWatcher;
+          });
+          if (isKcdWatchCountWatcher) {
+            watcherCount = watcherCount - 1;
           }
           angular.forEach(element.children(), function (childElement) {
             getWatchers(angular.element(childElement));
@@ -36,6 +36,14 @@ angular.module('kcd.site').directive('kcdWatchCount', function() {
         getWatchers(root);
 
         return watcherCount;
+      }
+
+      function getWatchersFromScope(scope) {
+        if (scope) {
+          return scope.$$watchers || [];
+        } else {
+          return [];
+        }
       }
     }
   };
