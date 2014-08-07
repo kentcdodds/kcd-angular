edb221a2304066883e85e8a0f37e36a865e501e2
Kent C. Dodds <kent@doddsfamily.us>
2014-08-06 23:36:08 -0600

    Reducing watch count just because I can...

--- resources/site_components/ThingCtrl/ThingCtrl.js
+++ resources/site_components/ThingCtrl/ThingCtrl.js
@@ -1,4 +1,6 @@
 angular.module('kcd.site').controller('ThingCtrl', function($scope, thing) {
   'use strict';
   $scope.thing = thing;
+  $scope.isLibrary = !!thing.libraryLink;
+  $scope.hasExample = !!thing.example;
 });
