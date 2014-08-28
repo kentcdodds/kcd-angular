0d5a0a0df21eaf8b54f1781f811639782bef7079
Kent C. Dodds <kentcdodds@attask.com>
2014-08-28 09:55:18 -0600

    Auto-generated commit

--- resources/site_components/ThingCtrl/ThingCtrl.js
+++ resources/site_components/ThingCtrl/ThingCtrl.js
@@ -1,8 +1,15 @@
-angular.module('kcd.site').controller('ThingCtrl', function($scope, thing, MochaService) {
+angular.module('kcd.site').controller('ThingCtrl', function($scope, thing, MochaService, $anchorScroll, $location, $timeout) {
   'use strict';
   $scope.thing = thing;
   $scope.isLibrary = !!thing.libraryLink;
   $scope.hasExample = !!thing.example;
   $scope.hasTests = !!thing.spec;
   $scope.testOnClear = MochaService.clearSuites;
+  $scope.scrollTo = function scrollTo(id) {
+    $location.hash(id);
+    $anchorScroll();
+  };
+  $timeout(function() {
+    $anchorScroll();
+  }, 500); // wait for a few digests to run to get the height to its max
 });
