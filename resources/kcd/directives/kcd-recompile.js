7ef901836781edb190443421ec31f97f475c286d
Roel Sint <rmsint@users.noreply.github.com>
2015-08-13 09:30:33 +0200

    Use spaces

--- resources/kcd/directives/kcd-recompile.js
+++ resources/kcd/directives/kcd-recompile.js
@@ -1,43 +1,43 @@
 angular.module('kcd.directives').directive('kcdRecompile', ['$parse', function($parse) {
-	'use strict';
-	return {
-		transclude: true,
-		link: function link(scope, $el, attrs, ctrls, transclude) {
-			var previousElements;
+  'use strict';
+  return {
+    transclude: true,
+    link: function link(scope, $el, attrs, ctrls, transclude) {
+      var previousElements;
 
-			compile();
+      compile();
 
-			function compile() {
-				transclude(scope, function(clone, clonedScope) {
-					// transclude creates a clone containing all children elements;
-					// as we assign the current scope as first parameter, the clonedScope is the same
-					previousElements = clone;
-					$el.append(clone);
-				});
-			}
+      function compile() {
+        transclude(scope, function(clone, clonedScope) {
+          // transclude creates a clone containing all children elements;
+          // as we assign the current scope as first parameter, the clonedScope is the same
+          previousElements = clone;
+          $el.append(clone);
+        });
+      }
 
-			function recompile() {
-				if (previousElements) {
-					previousElements.remove();
-					previousElements = null;
-					$el.empty();
-				}
+      function recompile() {
+        if (previousElements) {
+          previousElements.remove();
+          previousElements = null;
+          $el.empty();
+        }
 
-				compile();
-			}
+        compile();
+      }
 
-			scope.$watch(attrs.kcdRecompile, function(_new, _old) {
-				var useBoolean = attrs.hasOwnProperty('useBoolean');
-				if ((useBoolean && (!_new || _new === 'false')) || (!useBoolean && (!_new || _new === _old))) {
-					return;
-				}
-				// reset kcdRecompile to false if we're using a boolean
-				if (useBoolean) {
-					$parse(attrs.kcdRecompile).assign(scope, false);
-				}
+      scope.$watch(attrs.kcdRecompile, function(_new, _old) {
+        var useBoolean = attrs.hasOwnProperty('useBoolean');
+        if ((useBoolean && (!_new || _new === 'false')) || (!useBoolean && (!_new || _new === _old))) {
+          return;
+        }
+        // reset kcdRecompile to false if we're using a boolean
+        if (useBoolean) {
+          $parse(attrs.kcdRecompile).assign(scope, false);
+        }
 
-				recompile();
-			});
-		}
-	};
+        recompile();
+      });
+    }
+  };
 }]);
