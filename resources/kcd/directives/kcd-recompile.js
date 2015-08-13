5d87a88c160284e0404d6c33d4c8380fd33aef8f
Roel Sint <rmsint@users.noreply.github.com>
2015-08-13 09:21:39 +0200

    kcd-recompile on ng-repeat
    Follow up of #4
    
    This allows to use kcd-recompile on the same element as ng-repeat.

--- resources/kcd/directives/kcd-recompile.js
+++ resources/kcd/directives/kcd-recompile.js
@@ -1,33 +1,43 @@
-angular.module('kcd.directives').directive('kcdRecompile', function($compile, $parse) {
-  'use strict';
-  return {
-    scope: true, // required to be able to clear watchers safely
-    compile: function(el) {
-      var template = getElementAsHtml(el);
-      return function link(scope, $el, attrs) {
-        var stopWatching = scope.$parent.$watch(attrs.kcdRecompile, function(_new, _old) {
-          var useBoolean = attrs.hasOwnProperty('useBoolean');
-          if ((useBoolean && (!_new || _new === 'false')) || (!useBoolean && (!_new || _new === _old))) {
-            return;
-          }
-          // reset kcdRecompile to false if we're using a boolean
-          if (useBoolean) {
-            $parse(attrs.kcdRecompile).assign(scope.$parent, false);
-          }
+angular.module('kcd.directives').directive('kcdRecompile', ['$parse', function($parse) {
+	'use strict';
+	return {
+		transclude: true,
+		link: function link(scope, $el, attrs, ctrls, transclude) {
+			var previousElements;
 
-          // recompile
-          var newEl = $compile(template)(scope.$parent);
-          $el.replaceWith(newEl);
+			compile();
 
-          // Destroy old scope, reassign new scope.
-          stopWatching();
-          scope.$destroy();
-        });
-      };
-    }
-  };
+			function compile() {
+				transclude(scope, function(clone, clonedScope) {
+					// transclude creates a clone containing all children elements;
+					// as we assign the current scope as first parameter, the clonedScope is the same
+					previousElements = clone;
+					$el.append(clone);
+				});
+			}
 
-  function getElementAsHtml(el) {
-    return angular.element('<a></a>').append(el.clone()).html();
-  }
-});
+			function recompile() {
+				if (previousElements) {
+					previousElements.remove();
+					previousElements = null;
+					$el.empty();
+				}
+
+				compile();
+			}
+
+			scope.$watch(attrs.kcdRecompile, function(_new, _old) {
+				var useBoolean = attrs.hasOwnProperty('useBoolean');
+				if ((useBoolean && (!_new || _new === 'false')) || (!useBoolean && (!_new || _new === _old))) {
+					return;
+				}
+				// reset kcdRecompile to false if we're using a boolean
+				if (useBoolean) {
+					$parse(attrs.kcdRecompile).assign(scope, false);
+				}
+
+				recompile();
+			});
+		}
+	};
+}]);
